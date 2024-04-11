c16fb0391fab51289cede5cc2bad961a7c937a1f
PikachuMJ <78139496+PikachuMJ@users.noreply.github.com>
2024-04-11 10:57:37 +0200

    Boolean Arrays für mehrspieler

--- Kniffel.java
+++ Kniffel.java
@@ -4,14 +4,17 @@ public class Kniffel
 {
     //Array speichert Verfügbarkeit der Knöpfe in AuswahlFenster
     //  (hab bis jetzt keine bessere lösung gefunden)
-    static boolean[][] buttonAvailability = new boolean[2][13];
+    static boolean[] buttonAvailP1 = new boolean[13];
+    static boolean[] buttonAvailP2 = new boolean[13];
+
     //speichert Anzahl der Knopfdrücke die in MyFrame passieren
     static int buttonPressCount2 = 0;
 
     public static void main(String[] kniffelDings)
     {
         //setzt alle knöpfe als verfügbar
-        Arrays.fill(buttonAvailability, true);
+        Arrays.fill(buttonAvailP1, true);
+        Arrays.fill(buttonAvailP2, true);
         //allgemein startet alles
 
         new WuerfelFenster();
