a00cd526f89ec64f7a9c6b3fa363908dd52ac7e4
Marc <mj_redfox@outlook.de>
2024-04-14 16:06:31 +0200

    Kommentare

--- AnimierteBorder.java
+++ AnimierteBorder.java
@@ -13,7 +13,7 @@ public class AnimierteBorder implements Border
     }
 
     @Override
-    //allgemein aus Stackoverflow.
+    //basically erstellt die border drum rum
     public void paintBorder(Component c, Graphics g, int x, int y, int width, int height)
     {
         g.setColor(farbe);
@@ -24,17 +24,21 @@ public class AnimierteBorder implements Border
     }
 
     @Override
+    //einfach gesagt sagt es aus wie breit die border ist
     public Insets getBorderInsets(Component c)
     {
         return new Insets(breite, breite, breite, breite);
     }
 
     @Override
+    //macht nichts, wegen dem interface muss sie aber trotzdem da sein
     public boolean isBorderOpaque()
     {
         return true;
     }
     public void nextColor()
+    //einfach gesagt, geht durch bestimmte farben spektren, wenn
+    //bestimmte farben erreicht sind, wodurch die animation entsteht
     {
         int r = farbe.getRed();
         int g = farbe.getGreen();
@@ -59,7 +63,7 @@ public class AnimierteBorder implements Border
         {
             b -= 15;
         }
-
+        //updates die farbe und schaut das die farbe zwischen 0 und 255 ist
         farbe = new Color(Math.min(Math.max(r, 0), 255), Math.min(Math.max(g, 0), 255), Math.min(Math.max(b, 0), 255));
     }
 }
